2287aad44f97bb604be8ad96c98939c78c6afa85
Ana Maria <>
2015-01-29 11:15:15 -0500

    Se sube el proyecto y se hace commit a una clase Ejemplo para probar configuracion de Git

---
+++ ExploradorMarte/src/main/java/co/edu/unbosque/swii/marte/exploradormarte/Ejemplo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unbosque.swii.marte.exploradormarte;
+
+/**
+ *
+ * @author abarriosnuevo
+ */
+public class Ejemplo {
+    
+}
