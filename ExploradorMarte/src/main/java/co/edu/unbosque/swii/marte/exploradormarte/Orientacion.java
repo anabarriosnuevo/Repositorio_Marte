161a1c1b5b57138a26ac041f6bdebdf786ad7d69
abarriosnuevo <abarriosnuevo@ITSEMILLERO-08.itcol.com>
2015-01-29 12:03:53 -0500

    Se sube el prpyecto con las clases del ejemplo desde el aula

---
+++ ExploradorMarte/src/main/java/co/edu/unbosque/swii/marte/exploradormarte/Orientacion.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unbosque.swii.marte.exploradormarte;
+
+/**
+ *
+ * @author E205-1
+ */
+public enum Orientacion {
+    N,S,E,W;
+}
---
+++ ExploradorMarte/src/test/java/co/edu/unbosque/explorador/marte/test/MarteTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unbosque.explorador.marte.test;
+
+import co.edu.unbosque.swii.marte.exploradormarte.Marte;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author E205-1
+ */
+public class MarteTest {
+    
+    @Test
+    public void deberiaConfiurarLaCoordenadaMaxima(){
+        Marte m=new Marte(10,10);
+        Assert.assertEquals(10, m.getX());
+        Assert.assertEquals(10, m.getY());
+    }
+    
+   @Test(expectedExceptions = IllegalArgumentException.class)
+   public void noDebeRecibirValoresNegativosEnElConstructor(){
+       Marte m=new Marte(-2, -1);
+   }
+}
