b9dc69be94e496d999487455bb011099dfb92191
abarriosnuevo <abarriosnuevo@ITSEMILLERO-08.itcol.com>
2015-01-29 15:50:45 -0500

    Se corrige identacion, nombre del metodo, comentarios

--- ExploradorMarte/src/main/java/co/edu/unbosque/swii/marte/exploradormarte/Lector.java
+++ ExploradorMarte/src/main/java/co/edu/unbosque/swii/marte/exploradormarte/Lector.java
@@ -1,7 +1,7 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this template, choose Tools | Templates
+* and open the template in the editor.
+*/
 package co.edu.unbosque.swii.marte.exploradormarte;
 
 import java.io.BufferedReader;
@@ -18,7 +18,7 @@ import javax.imageio.IIOException;
  * @author E205-1
  */
 public class Lector {
-
+    
     File archivoJuego;
     
     BufferedReader br;
@@ -35,33 +35,33 @@ public class Lector {
         this.archivoJuego=new File(archivo);
         if(!archivoJuego.exists()){
             noExiste(archivo);
-        }else{       
-        FileInputStream fin=new FileInputStream(archivoJuego);
-        InputStreamReader isr=new InputStreamReader(fin);
-        br=new BufferedReader(isr);
+        }else{
+            FileInputStream fin=new FileInputStream(archivoJuego);
+            InputStreamReader isr=new InputStreamReader(fin);
+            br=new BufferedReader(isr);
         }
     }
-
+    
     
     public Posicion getLimiteSuperior() throws IOException {
         if(limiteSuperior==null)
-         limiteSuperior=br.readLine().trim();
+            limiteSuperior=br.readLine().trim();
         String x[]=limiteSuperior.split(" ");
-       return new Posicion(new Integer(x[0]),new Integer(x[1]));
+        return new Posicion(new Integer(x[0]),new Integer(x[1]));
     }
-
+    
     public Posicion getOrientacion() throws IOException {
         String x[]=lineaOrientacion.split(" ");
         Posicion p=new Posicion(new Integer(x[0]),new Integer(x[1]));
         p.setOrientacion(Orientacion.valueOf(x[2]));
         return p;
     }
-
+    
     public String getComandos() throws IOException {
         return this.lineaComandos;
     }
-
-   
+    
+    
     public boolean readNextExplorador() throws IOException{
         getLimiteSuperior();
         lineaOrientacion=br.readLine();
@@ -70,19 +70,20 @@ public class Lector {
         lineaComandos=br.readLine().trim();
         return true;
     }
- /**
- *
- * @author david Romero 29/01/2015
- */
-         public void noExiste(String archivo)  {// metodo que comprueba que existe el archivo 
-      this.archivoJuego = new File(archivo);
-      
-      if(!archivoJuego.exists())
+    /**
+     *
+     * @author david Romero 29/01/2015
+     * Metodo que comprueba que No existe el archivo
+     */
+    public void noExiste(String archivo)  {
+        this.archivoJuego = new File(archivo);
+        
+        if(!archivoJuego.exists())
         {
             System.out.println("El fichero no existe");
         }
- 
-      }  
+        
+    }
     
 }
 
--- ExploradorMarte/src/test/java/co/edu/unbosque/explorador/marte/test/LectorTest.java
+++ ExploradorMarte/src/test/java/co/edu/unbosque/explorador/marte/test/LectorTest.java
@@ -1,7 +1,7 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this template, choose Tools | Templates
+* and open the template in the editor.
+*/
 package co.edu.unbosque.explorador.marte.test;
 
 import co.edu.unbosque.swii.marte.exploradormarte.Lector;
@@ -13,14 +13,15 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
- *
  * @author E205-1
  */
 public class LectorTest {
     
+    String archivo = "D:/SemestreVIII/IngeSofII/juego.txt";
+    
     @Test
     public void probarLecturaPrimerLinea() throws IOException{
-        Lector l=new Lector("C:/Users/E205-1/Documents/sii/juego.txt"); 
+        Lector l=new Lector(archivo);
         Posicion limite=l.getLimiteSuperior();
         Assert.assertTrue(limite.getX()>0);
         Assert.assertTrue(limite.getY()>0);
@@ -28,36 +29,37 @@ public class LectorTest {
     
     @Test
     public void pruebaLaConstruccionDelLector() throws FileNotFoundException{
-       Lector l=new Lector("C:/Users/E205-1/Documents/sii/juego.txt");        
+        Lector l=new Lector(archivo);
     }
     
     @Test
     public void pruebaleerOrientacion() throws FileNotFoundException, IOException{
-       Lector l=new Lector("C:/Users/E205-1/Documents/sii/juego.txt"); 
-       l.readNextExplorador();
-       Posicion p=l.getOrientacion();
-       Assert.assertEquals(p.getX(),2);
-       Assert.assertEquals(p.getY(),2);
-       Assert.assertEquals(p.getOrientacion(), Orientacion.N);
+        Lector l=new Lector(archivo);
+        l.readNextExplorador();
+        Posicion p=l.getOrientacion();
+        Assert.assertEquals(p.getX(),2);
+        Assert.assertEquals(p.getY(),2);
+        Assert.assertEquals(p.getOrientacion(), Orientacion.N);
     }
     
     @Test
     public void pruebaleerComandos() throws FileNotFoundException, IOException{
-       Lector l=new Lector("C:/Users/E205-1/Documents/sii/juego.txt"); 
-       l.readNextExplorador();
-       String s=l.getComandos();
-       Assert.assertEquals(s,"IAIIAAA");
+        Lector l=new Lector(archivo);
+        l.readNextExplorador();
+        String s=l.getComandos();
+        Assert.assertEquals(s,"IAIIAAA");
     }
     
-        
+    
     /**
- *
- * @author david Romero 29/01/2015
- */
-        @Test
-    public void noexiste() throws FileNotFoundException, IOException{
-        Lector leer = new Lector("C:/Users/Andres/Desktop/exploradoe.txt");        
+     *
+     * @author david Romero 29/01/2015
+     * Metodo que verifica si existe o no el archivo
+     */
+    @Test
+    public void noExisteArchivo() throws FileNotFoundException, IOException{
+        Lector leer = new Lector(archivo);
     }
-
+    
     
 }
